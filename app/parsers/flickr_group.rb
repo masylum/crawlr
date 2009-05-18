a3a160185c536cfec61298676e9c2a5dc683be80
Pau Ramon <masylum@gmail.com>
2009-05-18 02:21:14 +0200

    Missing file on last commit

---
+++ app/parsers/flickr_group.rb
@@ -0,0 +1,14 @@
+class FlickrGroup < Parser
+  def parse_gallery(page)
+    page.search("//p[@class='PoolList']").each do |photo|
+      link = photo.at('a')
+      
+      description = link.attributes['title'].text
+      name = photo.search('a')[1].text
+      permalink = @url+link.attributes['href'].text
+      image = photo.at('img').attributes['src'].text.gsub(/_t\./, '.')
+      
+      create_gallery(name, description, permalink, image)
+    end
+  end
+end
